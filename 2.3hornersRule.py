9e690efdb2cd9d88fb24c30f3a52fccd91081e92
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2023-08-06 17:26:22 +0530

    Update 2.3hornersRule.py

--- 2.3hornersRule.py
+++ 2.3hornersRule.py
@@ -1,9 +1,15 @@
-def horner(poly, n, x):
-    result = poly[0] 
-    for i in range(1, n):
-        result = result*x + poly[i] 
+def horner_rule(coeffs, x):
+    n = len(coeffs)
+    result = coeffs[n - 1]
+
+    for i in range(n - 2, -1, -1):
+        result = result * x + coeffs[i]
+
     return result
-  
-#poly is coefficient of polymonials 
-#n in len of poly 
-#x is highest degree of polymonial 
+
+# Example usage:
+if __name__ == "__main__":
+    coeffs = [3, -6, 5, -2]  # Polynomial coefficients: 3x^3 - 6x^2 + 5x - 2
+    x = 2
+    result = horner_rule(coeffs, x)
+    print(f"The value of the polynomial at x={x} is:", result)
