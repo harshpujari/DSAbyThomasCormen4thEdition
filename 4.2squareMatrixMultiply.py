7094fc5f68fa9a8c360ad9c12fbbdd8a79441d3e
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2023-08-28 11:45:17 +0530

    Update 4.2squareMatrixMultiply.py

--- 4.2squareMatrixMultiply.py
+++ 4.2squareMatrixMultiply.py
@@ -1,8 +1,25 @@
 #Strassen’s algorithm for matrix multiplication
 #There are 2 more approaches for the same problme but not of much use, all are of O(n3)
-def multiply(A, B, C):  
-    for i in range(N):     
-        for j in range( N):          
-            C[i][j] = 0
-            for k in range(N):            
-                C[i][j] += A[i][k]*B[k][j]
+
+def square_matrix_multiply(A, B):
+    n = len(A)
+    C = [[0 for _ in range(n)] for _ in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            cij = 0
+            for k in range(n):
+                cij += A[i][k] * B[k][j]
+            C[i][j] = cij
+
+    return C
+
+# Example usage
+A = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+B = [[9, 8, 7], [6, 5, 4], [3, 2, 1]]
+
+result = square_matrix_multiply(A, B)
+
+for row in result:
+    print(row)
+
