bd894954a69649abf55818fd349c273fb92b27fa
Harsh Pujari <42710594+harshpujari@users.noreply.github.com>
2026-07-06 00:02:44 +0000

    DSA

--- 2 Getting Started/2.1insertionSort.py
+++ 2 Getting Started/2.1insertionSort.py
@@ -1,13 +1,17 @@
 def insertion_sort(arr):
+    # Traverse through 1 to len(arr)-1
     n = len(arr)
     for i in range(1, n):
         key = arr[i]
         j = i - 1
 
+        # Move elements of arr[0..i-1], that are greater than key,
+        # to one position ahead of their current position
         while j >= 0 and arr[j] > key:
             arr[j + 1] = arr[j]
             j -= 1
 
+        # Insert the key at the correct sorted position
         arr[j + 1] = key
 
 # Example usage:
